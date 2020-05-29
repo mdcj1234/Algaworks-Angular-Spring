c51a4ed0495dc9adfc4e7d03cc11ed63b6a30ef6
Marcio Junior <mdcj1234@gmail.com>
2020-05-28 22:20:16 -0300

    First commit
    My first commit

---
+++ backend/algamoney-api/src/main/java/com/algamoney/api/security/AppUserDetailsService.java
@@ -0,0 +1,41 @@
+package com.algamoney.api.security;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.algamoney.api.model.Usuario;
+import com.algamoney.api.repository.UsuarioRepository;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
+		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("usuário e/ou senha incorretos"));
+		
+		return new User(email, usuario.getSenha(), getPermissoes(usuario));
+	}
+
+	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+		Set<SimpleGrantedAuthority> authoroties = new HashSet<>();
+		usuario.getPermissoes().forEach(p -> authoroties.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
+		return authoroties;
+	}
+
+}
